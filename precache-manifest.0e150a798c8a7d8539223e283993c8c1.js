987fabbc240a82c1844ad90566ee4979d7382b3f
fancy <1186710960@qq.com>
2021-09-17 14:45:32 +0800

    deploy

---
+++ precache-manifest.0e150a798c8a7d8539223e283993c8c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733160298c30baec58f9",
+    "url": "/mango-website/css/app.83fd2f82.css"
+  },
+  {
+    "revision": "2c27fde61e4bc13266838d209f302c56",
+    "url": "/mango-website/index.html"
+  },
+  {
+    "revision": "733160298c30baec58f9",
+    "url": "/mango-website/js/app.726f4b6c.js"
+  },
+  {
+    "revision": "215e21c3acb629f1dec0",
+    "url": "/mango-website/js/chunk-vendors.84b0e46c.js"
+  },
+  {
+    "revision": "28a03ee60c6dc36a370ef5154535c651",
+    "url": "/mango-website/manifest.json"
+  },
+  {
+    "revision": "55e105bb530a5670a22d0d8d4f7fda81",
+    "url": "/mango-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mango-website/robots.txt"
+  }
+]);
